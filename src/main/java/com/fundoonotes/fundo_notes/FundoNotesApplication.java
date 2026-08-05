e187540da45bb3f70cae431f32f070886b5f26c9
Somya Rawat <somyarawat2005@gmail.com>
2026-08-05 22:42:17 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/fundoonotes/fundo_notes/FundoNotesApplication.java
@@ -0,0 +1,13 @@
+package com.fundoonotes.fundo_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundoNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundoNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fundoonotes/fundo_notes/FundoNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundoonotes.fundo_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundoNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
